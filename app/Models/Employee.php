966b7a6f688fd23af689842456a8876c87cadf9d
AnetaKan <aneta.kandic@sistemi.rs>
2023-05-23 14:39:09 +0200

    edited employees

--- app/Models/Employee.php
+++ app/Models/Employee.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Employee extends Model
 {
-    protected $table = 'employee';
+    protected $table = 'employees';
     protected $primaryKey = 'employee_id';
     public $timestamps = false;
 
